6c99d76d47befa39f54bfd007142a442a45b5426
milad.xsar23 <milad.xsar72@gmail.com>
2022-05-01 10:27:25 +0430

    create "createStore" function

---
+++ src/Redux.js
@@ -0,0 +1,11 @@
+"use strict";
+// createStore<Func>(reducer<pure function>) -> return Store<Object> -> use factory function pattern
+function createStore(reducer) {
+  let _state;
+  const getState = () => _state;
+//   fire and forget
+  const dispatch = (action) => {
+    _state = reducer(_state, action);
+  };
+  return { dispatch, getState };
+}
