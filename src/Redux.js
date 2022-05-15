606d56fb98e38863899de87f4aa1b85a6c9570b7
milad.xsar23 <milad.xsar72@gmail.com>
2022-05-15 09:58:45 +0430

    add combine reducer

--- src/Redux.js
+++ src/Redux.js
@@ -7,16 +7,25 @@ function createStore(reducer, initialState) {
   //   fire and forget
   const dispatch = (action) => {
     _state = reducer(_state, action);
-    _listeners.forEach(listenerFn => {
-      listenerFn()
-    })
+    _listeners.forEach((listenerFn) => {
+      listenerFn();
+    });
   };
   const subscribe = (listenerFn) => {
-    _listeners.push(listenerFn)
-  }
+    _listeners.push(listenerFn);
+  };
   return { dispatch, getState, subscribe };
 }
 
+function combineReducers(combineObject) {
+  function rootReducer(state, action) {
+    let result = {};
+    for (let key of Object.keys(combineObject)) {
+      result[key] = combineObject[key](state, action);
+    }
+    return result;
+  }
+  return rootReducer;
+}
 
-
-export {createStore}
+export { createStore, combineReducers };
